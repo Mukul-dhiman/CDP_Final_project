2c0dc1eca5ba91bf60b135a233fbc05c1aab0cb0
Mukul-dhiman <orton1615@gmail.com>
2020-11-20 01:22:24 +0530

    working multi clients

--- Server.c
+++ Server.c
@@ -14,10 +14,6 @@ void error(const char *msg){
   exit(1);
 }
 
-struct info{
-	int val;
-};
-
 //socket for servers
 int lstnsockfd; 
 
@@ -34,84 +30,90 @@ void server(int consockfd) {
   }
 }
 
-static void* go(void* arg){
-	int n;
-	struct info *temp = arg;
-	n=temp->val;
 
-	printf("Hello from thread %d\n" , n );
+struct info{
+	int val;
+};
+
+static void* go(void* arg){
+	int thread_id;
+	struct info *temp = arg;
+	thread_id=temp->val;
+
+	printf("Hello from thread %d\n" , thread_id );
 
   int consockfd, clilen;
   struct sockaddr_in cli_addr;
-  while (1) {
-   printf("Listening for incoming connections\n");
+  while(1){
+    printf("Listening for incoming connections\n");
 
-/* Listen for incoming connections */
-   listen(lstnsockfd, MAXQUEUE); 
+    /* Listen for incoming connections */
+    listen(lstnsockfd, MAXQUEUE); 
 
   //  clilen = sizeof(cl_addr);
 
-/* Accept incoming connection, obtaining a new socket for it */
-   consockfd = accept(lstnsockfd, (struct sockaddr *) &cli_addr,       
-                      &clilen);
+  /* Accept incoming connection, obtaining a new socket for it */
+    consockfd = accept(lstnsockfd, (struct sockaddr *) &cli_addr,&clilen);
 
     if(consockfd<0){
       error("Error on Accepting");
     }
-   printf("Accepted connection\n");
+    printf("Accepted connection\n");
 
-   server(consockfd);
+    server(consockfd);
 
-   close(consockfd);
+    close(consockfd);
   }
   
-	pthread_exit(&(n)+100);  //(int*)(100 + n)
+	pthread_exit(&(thread_id)+100);  //(int*)(100 + n)
 	return NULL;
 	// Not reached
 }
+
 
 static pthread_t threads[MAXQUEUE];
 
 int main(int argc,char **argv){
 
-int portno = 5033;
-struct sockaddr_in serv_addr, cli_addr;
+  int portno = 5033;
+  struct sockaddr_in serv_addr, cli_addr;
 
- memset((char *) &serv_addr,0, sizeof(serv_addr));
- serv_addr.sin_family      = AF_INET;
- serv_addr.sin_addr.s_addr = INADDR_ANY;
- serv_addr.sin_port        = htons(portno);
+  memset((char *) &serv_addr,0, sizeof(serv_addr));
+  serv_addr.sin_family      = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port        = htons(portno);
 
-// Server protocol
-/* Create Socket to receive requests*/
-lstnsockfd = socket(AF_INET, SOCK_STREAM, 0);
+  // Server protocol
+  /* Create Socket to receive requests*/
+  lstnsockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-if(lstnsockfd<0){
-  error("Error opening Sockets.");
-}
+  if(lstnsockfd<0){
+    error("Error opening Sockets.");
+  }
 
-/* Bind socket to port */
-if(bind(lstnsockfd, (struct sockaddr *)&serv_addr,sizeof(serv_addr))<0){
-  error("binding failed");
-}
-printf("Bounded to port\n");
+  /* Bind socket to port */
+  if(bind(lstnsockfd, (struct sockaddr *)&serv_addr,sizeof(serv_addr))<0){
+    error("binding failed");
+  }
+  printf("Bounded to port\n");
+
+  //Passing struct for Thread id
+  struct info thread_id_struct[MAXQUEUE];
+  int ii;
+  for( ii = 0; ii < MAXQUEUE ; ii++){
+    thread_id_struct[ii].val=ii;
+  }
+  //Threads for queueing MAXQUEUE number of clients
+  for( ii = 0; ii < MAXQUEUE ; ii++){
+	  pthread_create(&(threads[ii]),NULL,&go,&thread_id_struct[ii]);
+  }
+  long ret;
+  for ( ii = 0; ii < MAXQUEUE ; ii++){
+	  ret = pthread_join(threads[ii],(void**)&ret);
+	  printf("Thread %d returned %ld\n" , ii , ret );
+  }
+  printf( "Main thread done.\n" );
 
 
-
-//Threads for queueing MAXQUEUE number of clients
-int ii;
-for ( ii = 0; ii < MAXQUEUE ; ii++){
-	struct info *temp;
-	temp->val=ii;
-	pthread_create(&(threads[ii]),NULL,&go,temp);
-}
-long ret;
-for ( ii = 0; ii < MAXQUEUE ; ii++){
-	ret = pthread_join(threads[ii],(void**)&ret);
-	printf("Thread %d returned %ld\n" , ii , ret );
-}
-printf( "Main thread done.\n" );
-
-
-close(lstnsockfd);
+  close(lstnsockfd);
 }
